cabfa2ca7121166183772573232ca6b04abc32e9
OOPer <nagata@shlab.jp>
2017-05-22 03:18:29 +0900

    Fixed @escaping bug

--- iPublicUtility/CAXException.swift
+++ iPublicUtility/CAXException.swift
@@ -116,7 +116,7 @@ class CAXException: OOPException {
         sWarningHandler?(s, error)
     }
     
-    class func setWarningHandler(_ f: WarningHandler) { sWarningHandler = f }
+    class func setWarningHandler(_ f: @escaping WarningHandler) { sWarningHandler = f }
     private static var sWarningHandler: WarningHandler? = nil
     
     override var description: String {
